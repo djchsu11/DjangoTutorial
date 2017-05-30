53fe6bb9221c77214e52c0cb445497c0065f2ecf
messenger <or.no.world@gmail.com>
2017-05-30 17:43:11 -0500

    Added templates and some DB items. Fixed time zone issue - needs to be looked into in the future

--- polls/urls.py
+++ polls/urls.py
@@ -18,5 +18,11 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    #/polls/
     url(r'^$', views.index, name="index"),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    #/polls/number/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    #/polls/5/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
