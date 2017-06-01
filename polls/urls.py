6fb7ae94c0c8b828cda1705c5759a0a0197bf170
messenger <or.no.world@gmail.com>
2017-06-01 14:18:53 -0500

    generic views

--- polls/urls.py
+++ polls/urls.py
@@ -20,10 +20,10 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     #/polls/
-    url(r'^$', views.index, name="index"),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #/polls/number/results
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     #/polls/5/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
